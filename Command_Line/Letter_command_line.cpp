8af797f959a95074868982bee51ef9d6bb93bc71
pravinzode <128887002+pravinzode@users.noreply.github.com>
2023-05-02 10:17:33 +0530

    Add files via upload

---
+++ Command_Line/Letter_command_line.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+	cout<<"Hello "<<argv[1]<<","<<endl<<endl;
+	cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl;
+	cout <<"xxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl;
+	cout<<endl<<endl;
+	cout <<"             yours sicererly"<<endl;
+	cout<<"              "<<argv[1]<<" "<<argv[2]<<endl;
+return 0;
+}
+
+
+
---
+++ Command_Line/addition_cmd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+	int sum =(atoi(argv[1])+atoi(argv[2]));
+	cout<<sum<<endl;
+	
+return 0;
+}
+
+
+
